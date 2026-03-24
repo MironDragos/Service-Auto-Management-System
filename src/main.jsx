8d5d170ed7575e12f34c31ddb4fa86b5b993f7f7
MironDragos <mdragos098@gmail.com>
2026-03-24 15:38:26 +0200

    add: added a page for 404

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import CarDetails from './pages/CarDetails.jsx'
 import Cars from './pages/Cars.jsx'
 import AddCar from './pages/AddCar.jsx'
 import Settings from './pages/Settings.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
   {path: "/CarDetails", element: <CarDetails/>},
   {path: "/Cars", element: <Cars/>},
   {path: "/AddCar", element: <AddCar/>},
-  {path: "/Settings", element: <Settings/>}
+  {path: "/Settings", element: <Settings/>},
+  {path: "*", element: <NotFoundPage/>}
 ])
 
 createRoot(document.getElementById('root')).render(
